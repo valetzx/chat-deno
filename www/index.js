581231762a1980cd3e6866ece9b5001ab6aa524c
valetzx <45054400+valetzx@users.noreply.github.com>
2025-06-28 09:51:29 +0000

    del ws path

--- www/index.js
+++ www/index.js
@@ -1,5 +1,5 @@
 const wsProtocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
-const wsUrl = `${wsProtocol}://${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/ws`;
+const wsUrl = `${wsProtocol}://${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}`;
 
 var users = [];
 var me = new XChatUser();
